2a03b8d565c0742cf386cf347b812ab8b7800a7c
Paris Papadopoulos <parisppdo@protonmail.com>
2024-09-01 20:36:05 +0300

    created posts.php

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -36,7 +36,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#"> View all posts </a>
+                                <a href="./posts.php"> View all posts </a>
                             </li>
                             <li>
                                 <a href="#"> Add posts </a>
@@ -44,7 +44,7 @@
                         </ul>
                     </li>
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Lenses </a>
+                        <a href="./lenses.php"><i class="fa fa-fw fa-wrench"></i> Lenses </a>
                     </li>
                     <li class="active">
                         <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments </a>
---
+++ admin/posts.php
@@ -0,0 +1,57 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>                    
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Lens</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>fdsa</td>
+                                <td>fdsa</td>
+                                <td>afds</td>
+                                <td>afds</td>
+                                <td>afds</td>
+                                <td>afds</td>
+                                <td>asfd</td>
+                                <td>afds</td>
+                                <td>asfd</td>
+                            </tr>
+                        </tbody>  
+                        </table>  
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>
