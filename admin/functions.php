9a7afb21cab650d0c2da5b360a260e8d1178496c
Paris Papadopoulos <parisppdo@protonmail.com>
2024-09-01 14:54:07 +0300

    Refactoring-Created functions.php

---
+++ admin/functions.php
@@ -0,0 +1,48 @@
+<?php
+function add_lens($connection) {
+    //global $connection;
+    if (isset($_POST['submit'])) {
+        $lens_name = $_POST['lens_name'];
+        if ($lens_name == "" || empty($lens_name)) {
+            echo "This field should not be empty";
+        }
+        else {
+            $query = "INSERT INTO lenses(lens_name) ";
+            $query .= "VALUE('{$lens_name}')";
+
+            $create_lens_query = mysqli_query($connection, $query);
+
+            if (!$create_lens_query) {
+                die('QUERY FAILED' . mysqli_error($connection));
+            }
+        }
+    }
+}
+
+function find_all_lenses($connection) {
+    $query = "SELECT * FROM lenses";
+    $select_lenses = mysqli_query($connection, $query);
+    
+    while ($row = mysqli_fetch_assoc($select_lenses)) {
+        $lens_id = $row['lens_id'];
+        $lens_name = $row['lens_name'];
+
+        echo "<tr>";
+        echo "<td>{$lens_id}</td>";
+        echo "<td>{$lens_name}</td>";
+        echo "<td><a href='lenses.php?delete={$lens_id}'>Delete</a></td>";
+        echo "<td><a href='lenses.php?edit={$lens_id}'>Edit</a></td>";
+        echo "<tr>";
+    }
+}
+
+function delete_lens($connection) {
+    if (isset($_GET['delete'])) {
+        $lens_to_delete_id = $_GET['delete'];
+        $query = "DELETE FROM lenses WHERE lens_id = {$lens_to_delete_id}";
+        $delete_query = mysqli_query($connection, $query);
+        header("Location: lenses.php");
+    }
+}
+
+?>
--- admin/lenses.php
+++ admin/lenses.php
@@ -17,25 +17,7 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">                            
-                            <?php
-                                if (isset($_POST['submit'])) {
-                                    $lens_name = $_POST['lens_name'];
-                                    if ($lens_name == "" || empty($lens_name)) {
-                                        echo "This field should not be empty";
-                                    }
-                                    else {
-                                        $query = "INSERT INTO lenses(lens_name) ";
-                                        $query .= "VALUE('{$lens_name}')";
-
-                                        $create_lens_query = mysqli_query($connection, $query);
-
-                                        if (!$create_lens_query) {
-                                            die('QUERY FAILED' . mysqli_error($connection));
-                                        }
-                                    }
-
-                                }
-                            ?>
+                            <?php add_lens($connection); ?> <!--ADD LENSES QUERY-->
                             <form action="" method="post">
                                 <div class="form-group">
                                     <label for="lens_name">Add Lens</label>
@@ -46,7 +28,7 @@
                                 </div>
                             </form>
                             <?php
-                            
+                                // UPDATE QUERY
                                 if (isset($_GET['edit'])) {
                                     $lens_id = $_GET['edit'];
                                     include "includes/update_lenses.php";
@@ -63,33 +45,8 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <?php
-                                    // FIND ALL LENSES QUERY
-                                    $query = "SELECT * FROM lenses";
-                                    $select_lenses = mysqli_query($connection, $query);
-                                    
-                                    while ($row = mysqli_fetch_assoc($select_lenses)) {
-                                        $lens_id = $row['lens_id'];
-                                        $lens_name = $row['lens_name'];
-
-                                        echo "<tr>";
-                                        echo "<td>{$lens_id}</td>";
-                                        echo "<td>{$lens_name}</td>";
-                                        echo "<td><a href='lenses.php?delete={$lens_id}'>Delete</a></td>";
-                                        echo "<td><a href='lenses.php?edit={$lens_id}'>Edit</a></td>";
-                                        echo "<tr>";
-                                    }
-                                ?>
-                                <?php
-                                    if (isset($_GET['delete'])) {
-                                        $lens_to_delete_id = $_GET['delete'];
-                                        $query = "DELETE FROM lenses WHERE lens_id = {$lens_to_delete_id}";
-                                        $delete_query = mysqli_query($connection, $query);
-                                        header("Location: lenses.php");
-                                    }
-                                
-                                
-                                ?>
+                                <?php find_all_lenses($connection); ?> <!--FIND LENSES QUERY-->
+                                <?php delete_lens($connection); ?><!--DELETE LENSES QUERY-->
                                 </tbody>
                             </table>
                         </div>
