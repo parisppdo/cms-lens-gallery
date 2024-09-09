352e8def2d6622bc6b1830c01dd813af911ef22b
Paris Papadopoulos <parisppdo@protonmail.com>
2024-09-09 11:00:22 +0300

    added post status dropdown option in edit post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -60,6 +60,7 @@
         <label for="title">Post Title</label>
             <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
     </div>
+    
     <div class="form-group">
         <select name="post_lens_id" id="">
             <?php
@@ -77,29 +78,48 @@
             ?>
         </select>
     </div>
+    
     <div class="form-group">
         <label for="title">Post Author</label>
         <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
     </div>
+    
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
+        <label for="">Select Status</label>
+        <select name="post_status" id="">
+            <option value="<?php echo $post_status; ?>"><?php echo $post_status; ?></option>
+            <?php
+
+                if ($post_status == 'published') {
+                    echo "<option value='draft'>draft</option>";
+                }
+                else {
+                    echo "<option value='published'>published</option>";
+                }
+
+            ?>
+        </select>
     </div>
+
     <div class="form-group">
         <label for="post_image">Post Image</label>
     </div>
-        <div class="form-group">
+    
+    <div class="form-group">
         <img src="../images/<?php echo $post_image; ?>" width="100" alt="">
         <input type="file" name="image">
     </div>
+    
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
         <input value="<?php echo $post_tags; ?>"type="text" class="form-control" name="post_tags">
     </div>
+    
     <div class="form-group">
         <label for="post_content">Post Content</label>
         <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
     </div>
+    
     <div class="form-group">
         <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
     </div>
