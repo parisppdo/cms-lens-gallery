0b9d40151de2f30d059ebd8d2f8f309f65e979af
Paris Papadopoulos <parisppdo@protonmail.com>
2024-12-19 16:56:25 +0200

    created latest posts widget in sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -24,7 +24,7 @@
                             <input name="username" type="text" class="form-control" placeholder="Enter Username">
                         </div>
                         <div class="input-group">
-                            <input name="password" type="passwird" class="form-control" placeholder="Enter Password">
+                            <input name="password" type="password" class="form-control" placeholder="Enter Password">
                             <span class="input-group-btn">
                                 <button class="btn btn-primary" name="login" type="submit">Submit</button>
                             </span>
--- includes/widget.php
+++ includes/widget.php
@@ -1,4 +1,13 @@
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+<div class="well">
+    <h4>Latest Posts</h4>
+    <?php
+        $query = "SELECT * FROM posts ORDER BY post_id DESC LIMIT 5";
+        $select_latest_posts_query = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_assoc($select_latest_posts_query)) {
+            $post_title = $row['post_title'];
+            $post_author = $row['post_author'];
+            echo "<li>$post_title by $post_author</li>";
+        }
+    ?>
+</div>
